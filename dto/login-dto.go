211f6c8de1d650f979f93599d626f589124d888a
WilkerAlves <wilkeralves97@gmail.com>
2021-09-04 11:58:45 -0300

    teste da rota de login e register

--- dto/login-dto.go
+++ dto/login-dto.go
@@ -2,6 +2,6 @@ package dto
 
 //LoginDTO is used when client post from /login url
 type LoginDTO struct {
-	Email 		string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password 	string `json:"password,omitempty" form:"password,omitempty" validate:"min:6"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password" form:"password" binding:"required"`
 }
--- dto/register-dto.go
+++ dto/register-dto.go
@@ -2,7 +2,7 @@ package dto
 
 //RegisterDTO is used when client post from /register url
 type RegisterDTO struct {
-	Name 		string `json:"name" form:"name" binding:"required"`
-	Email 		string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password 	string `json:"password,omitempty" form:"password,omitempty" validate:"min:6"`
+	Name     string `json:"name" form:"name" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password,omitempty" form:"password,omitempty"`
 }
